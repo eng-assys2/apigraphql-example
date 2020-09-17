e9c54f3319b4b9dd41d7bcc129d014b3cce5bffb
Lucas Assis <eng.assys@gmail.com>
2020-09-17 18:20:43 +0000

    add mongoose and models

--- src/start-server.js
+++ src/start-server.js
@@ -1,6 +1,11 @@
 import { ApolloServer } from 'apollo-server'
+import mongoose from 'mongoose'
 
 function startServer({ typeDefs, resolvers }) {
+  mongoose.connect('mongodb://test:test@vollaredb:27017/graphql?authSource=admin', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   const server = new ApolloServer({ typeDefs, resolvers })
   server.listen().then(({ url }) => console.log(`🔥 Server started at ${url}`))
 }
